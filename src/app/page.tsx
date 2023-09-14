88dee700cf9330b796b5c21214af7a64a0590885
saku-1101 <sakupi1101@outlook.jp>
2023-09-14 21:29:18 +0900

    update: マウスの動きに合わせてラケットのように動くように調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,63 @@
 "use client";
 import { Floor } from "./components/Floor";
-import { RigidBody, CuboidCollider } from "@react-three/rapier";
+import { RigidBody, RapierRigidBody } from "@react-three/rapier";
 import { Ball } from "./components/Ball";
-import { Draggable } from "./utils/Draggable";
+import { useRef } from "react";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
 
 export default function Home() {
+  const rigidRef = useRef<RapierRigidBody>(null);
+  useFrame((state) => {
+    if (rigidRef.current) {
+      rigidRef.current.rotation().x = THREE.MathUtils.lerp(
+        rigidRef.current.rotation().x,
+        0,
+        0.2
+      );
+      const currentRotationX = THREE.MathUtils.lerp(
+        rigidRef.current.rotation().x,
+        0,
+        0.2
+      );
+      const currentRotationY = THREE.MathUtils.lerp(
+        rigidRef.current.rotation().y,
+        (state.mouse.x * Math.PI) / 5,
+        0.2
+      );
+      rigidRef.current.rotation().y = currentRotationY;
+      rigidRef.current.setTranslation(
+        { x: state.mouse.x * 10, y: state.mouse.y * 5, z: 0 },
+        true
+      );
+      rigidRef.current.setRotation(
+        {
+          x: currentRotationY, // x回転軸
+          y: currentRotationX, // y回転軸
+          z: 0, // z回転軸
+          w: 1, // 回転角
+        },
+        true
+      );
+    } else {
+      return;
+    }
+  });
   return (
     <>
       <RigidBody position={[0, 5, 0]} colliders="ball">
         <Ball />
       </RigidBody>
-      {/* <RigidBody
+      {/* <RigidBody /> component is used to add a mesh into the physics world. Automatically generate Colliders based on the shape of the wrapped meshes */}
+      {/* when we first run, the object inside will fall down because the gravity affects to it. */}
+      <RigidBody
         ref={rigidRef}
         type="kinematicVelocity"
         position={[0, 0, 0]}
         restitution={2}
-        friction={0}
-      > */}
-      {/* <RigidBody /> component is used to add a mesh into the physics world. Automatically generate Colliders based on the shape of the wrapped meshes */}
-      {/* when we first run, the object inside will fall down because the gravity affects to it. */}
-      {/* <Floor />
-      </RigidBody> */}
-      <Draggable>
-        <CuboidCollider
-          position={[0, 0, 0]}
-          args={[20, 0.5, 20]}
-          restitution={2}
-        />
-      </Draggable>
+      >
+        <Floor />
+      </RigidBody>
     </>
   );
 }
