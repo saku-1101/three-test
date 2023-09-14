296dba3e3e67f0ad57b3c76e62b9ace711a3a8e1
saku-1101 <sakupi1101@outlook.jp>
2023-09-14 21:22:22 +0900

    update: floorをdraggableに

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { Floor } from "./components/Floor";
-import { RigidBody } from "@react-three/rapier";
+import { RigidBody, CuboidCollider } from "@react-three/rapier";
 import { Ball } from "./components/Ball";
+import { Draggable } from "./utils/Draggable";
 
 export default function Home() {
   return (
@@ -9,11 +10,24 @@ export default function Home() {
       <RigidBody position={[0, 5, 0]} colliders="ball">
         <Ball />
       </RigidBody>
-      <RigidBody type="fixed" position={[0, 0, 0]} restitution={2}>
-        {/* <RigidBody /> component is used to add a mesh into the physics world. Automatically generate Colliders based on the shape of the wrapped meshes */}
-        {/* when we first run, the object inside will fall down because the gravity affects to it. */}
-        <Floor />
-      </RigidBody>
+      {/* <RigidBody
+        ref={rigidRef}
+        type="kinematicVelocity"
+        position={[0, 0, 0]}
+        restitution={2}
+        friction={0}
+      > */}
+      {/* <RigidBody /> component is used to add a mesh into the physics world. Automatically generate Colliders based on the shape of the wrapped meshes */}
+      {/* when we first run, the object inside will fall down because the gravity affects to it. */}
+      {/* <Floor />
+      </RigidBody> */}
+      <Draggable>
+        <CuboidCollider
+          position={[0, 0, 0]}
+          args={[20, 0.5, 20]}
+          restitution={2}
+        />
+      </Draggable>
     </>
   );
 }
